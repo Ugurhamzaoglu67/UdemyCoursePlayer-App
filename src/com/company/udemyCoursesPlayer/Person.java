424cc8b8bbdff34735a32eff71a2d6161466d0db
Ugurhamzaoglu67 <craxx67@gmail.com>
2021-05-31 23:22:31 +0300

    Person abstract class created

---
+++ src/com/company/udemyCoursesPlayer/Person.java
@@ -0,0 +1,48 @@
+package com.company.udemyCoursesPlayer;
+
+
+
+public  abstract class Person {
+    private String name;
+    private String userName;
+    private String whoAreu;
+    private int age;
+
+
+
+    public Person(String name, int age, String userName, String whoAreu) {
+        this.name = name;
+        this.age = age;
+        this.userName = userName;
+        this.whoAreu = whoAreu;
+    }
+
+
+
+    //GETTER
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getWhoAreu() {
+        return whoAreu;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", userName='" + userName + '\'' +
+                ", whoAreu='" + whoAreu + '\'' +
+                '}';
+    }
+}
